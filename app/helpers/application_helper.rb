9dc77995ff80764b39bd0ff363c7f22a5bc8d444
michael jacoby <michaelkjacoby@gmail.com>
2012-02-03 11:49:32 -0600

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+# Return a title on a perpage basis.
+
+def title
+   base_title = "Demo_App"
+   if @title.nil?
+     base_title
+	 else
+	 "#{base_title} | #{@title}"
+	end
+   end
 end
