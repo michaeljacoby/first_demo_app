a0e25c9a8fe1e14b897e2e014c0017fbc1826439
michael jacoby <michaelkjacoby@gmail.com>
2012-02-26 15:11:00 -0600

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-  def show 
-  @user = User.find(params[:id])
-  @title = @user.name
-  end
+
   
   def new
   @user = User.new
   @title = "Sign up!"
   end
+  
+  def show 
+  @user = User.find(params[:id])
+  @title = @user.name
+  end  
   
   
   
